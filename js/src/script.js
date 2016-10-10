090e25f02dda86123b7a782a97f5250980db8857
Doctor_Li <sirozhechka@mail.ru>
2016-10-10 22:01:32 +0300

    fixed

--- js/src/script.js
+++ js/src/script.js
@@ -27,18 +27,11 @@ $(function(){
   });
 });
 /*TASK 2*/
-alert('Задание 2 в консоли');
 $.getJSON("https://raw.githubusercontent.com/goit-fe/markup_fe2o/master/js_19-20/data.json", function(data){
   var newData = _.union(_.flatten(_.map(data, 'skills'))).sort();
-console.log('Skills:', newData);
-});
-
-$.getJSON("https://raw.githubusercontent.com/goit-fe/markup_fe2o/master/js_19-20/data.json", function(dataName){
-  dataName =_.map(_.sortBy(dataName, function (item){return item.friends.length;}), 'name');
+  var dataName =_.map(_.sortBy(data, function (item){return item.friends.length;}), 'name');
+  var dataFriends = _.union(_.map(_.flatten(_.map(data,'friends')), 'name'));
   console.log('Name:',dataName);
-});
-
-$.getJSON("https://raw.githubusercontent.com/goit-fe/markup_fe2o/master/js_19-20/data.json", function(dataFriends){
-  dataFriends = _.union(_.map(_.flatten(_.map(dataFriends,'friends')), 'name'));
+  console.log('Skills:', newData);
   console.log('Friends:',dataFriends);
 });
